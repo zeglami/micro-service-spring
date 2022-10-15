c4467257539da46bca85dfe6aa35dafea51f0ed5
zeglami <zeglami.abdelhamid@gmail.com>
2022-10-16 00:50:53 +0200

    add external api

--- gateway-service/src/main/java/me/zegit/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/me/zegit/gatewayservice/GatewayServiceApplication.java
@@ -16,9 +16,48 @@ public class GatewayServiceApplication {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
+
+    //routage static
+    //lb: load balancer
+    /*
+     * What Is Round-Robin Load Balancing? Round‑robin load balancing is one of
+     * the simplest methods for distributing client requests across a group of servers.
+     *  Going down the list of servers in the group, the round‑robin load balancer
+     * forwards a client request to each server in turn.
+     *
+     *  //.route(r -> r.path("/products/**").uri("lb://INVENTORY-SERVICE"))
+     *
+     * */
     @Bean
-    DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
-        return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+    RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+
+        return builder.routes()
+                .route(r -> r.path("/publicCities/**")
+                        .filters(f -> f
+                                .addRequestHeader("X-RapidAPI-Key", "2f2a449896msh690958a5bde4506p13c17cjsn65cf1e6106ed")
+                                .addRequestHeader("X-RapidAPI-Host", "wft-geo-db.p.rapidapi.com")
+                                .rewritePath("/publicCities/(?<segment>.*)", "/${segment}")
+
+                        )
+                        .uri("https://wft-geo-db.p.rapidapi.com/v1/geo/cities"))
+
+
+                .route(r -> r.path("/muslim/**")
+                        .filters(f -> f
+                                .addRequestHeader("X-RapidAPI-Key", "2f2a449896msh690958a5bde4506p13c17cjsn65cf1e6106ed")
+                                .addRequestHeader("X-RapidAPI-Host", "muslimsalat.p.rapidapi.com")
+                                .rewritePath("/muslim/(?<segment>.*)", "/${segment}")
+
+                        )
+                        .uri("https://muslimsalat.p.rapidapi.com"))
+
+                .build();
+    }
+
+
+    @Bean
+    DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp) {
+        return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
     }
 
 
